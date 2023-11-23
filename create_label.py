bb5dd322794329ab4ad63940634229619d6cc2c9
Kehang Zhu <72799310+KeHang-Zhu@users.noreply.github.com>
2023-11-22 23:54:25 -0500

    Update create_label.py

--- create_label.py
+++ create_label.py
@@ -1,4 +1,5 @@
 import time 
+import threading
 import jsonlines
 import os
 import openai
@@ -30,7 +31,7 @@ def getCodeFormat(code_input):
     '''
 
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo-1106",
+        model="gpt-4-1106-preview",
         messages=[
             {
                 "role": "system",
@@ -48,6 +49,7 @@ def getCodeFormat(code_input):
         presence_penalty=0
     )
     
+    print("Getting format")
     responseText = response.choices[0].message.content
 
     # Remove the ```python and ``` from the response
@@ -58,40 +60,52 @@ def getCodeFormat(code_input):
 
     return responseText
 
-def generateCode(modelType, userPrompt): 
-    # Models: "gpt-4-1106-preview", "gpt-3.5-turbo-1106"
-    response = client.chat.completions.create(
-    model=modelType,
-    messages=[
-        {
-            "role": "system",
-            "content": "You are an expert in writing Python code. You will generate a Python Script that will complete the task outlined by the user. You will not provide any explanations, and only return the Python Script without the explanation of the code. The most IMPORTANT note is to have precise indents in your output. It is IMPORTANT to only include python code, and nothing else. Your response should start with ```python and end with ```"
-        },
-        {
-            "role": "user",
-            "content": userPrompt
-        }
-    ],
-    temperature=1,
-    max_tokens=2047,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
+def call_api(modelType, userPrompt, response_container):
+    response_container["response"] = client.chat.completions.create(
+        model=modelType,
+        messages=[
+            {
+                "role": "system",
+                "content": "You are an expert in writing Python code. You will generate a Python Script that will complete the task outlined by the user. You will not provide any explanations, and only return the Python Script without the explanation of the code. The most IMPORTANT note is to have precise indents in your output. It is IMPORTANT to only include python code, and nothing else. Your response should start with ```python and end with ```"
+            },
+            {
+                "role": "user",
+                "content": userPrompt
+            }
+        ],
+        temperature=1,
+        max_tokens=2047,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
-    responseText = response.choices[0].message.content
+    
+def generateCode(modelType, userPrompt):
+    max_attempts = 1  # set max retrial times
+    for attempt in range(max_attempts):
+        response_container = {"response": None}
+        api_thread = threading.Thread(target=call_api, args=(modelType, userPrompt, response_container))
+        api_thread.start()
+        api_thread.join(timeout=10)  # set 10s
 
-    # Remove the ```python and ``` from the response
-    responseText = responseText.replace("```python", "")
-    responseText = responseText.replace("```", "")
+        if api_thread.is_alive():
+            print("Timeout, retrying...")
+            api_thread.join()  # complete the thread
+            continue
 
-    #Fix Spacing from responseTest so it can run on exec()
- 
+        response = response_container["response"]
+        if response:
+            print("... generating code", modelType)
+            responseText = response.choices[0].message.content
+            print("---Code generated")
 
+            # Remove the ```python and ``` from the response
+            responseText = responseText.replace("```python", "").replace("```", "")
 
+            # Fix Spacing from responseText so it can run on exec()
+            return responseText
 
-    return responseText
-
-
+    return "def wrongfunc(): \r\n\treturn 0"
 
 def checkCorrectness(output, obj):
         # Execute the code:
@@ -114,8 +128,8 @@ def checkCorrectness(output, obj):
     
 if __name__ == '__main__':
     # Create a text file storing progress   
-    #max_loop = 974
-    max_loop = 100
+    max_loop = 974
+    # max_loop = 100
     # See how many rows are in mbpp_label jsonl file
     with jsonlines.open('mbpp_label.jsonl') as reader:
         i = sum(1 for _ in reader)
@@ -137,7 +151,7 @@ if __name__ == '__main__':
             print("Running Example: " + str(i))
             prompt = obj['text']
             code = obj['code']
-            #print(obj['code'])
+            # print(obj['code'])
             # Call the getCodeFormat function to get the function call and its inputs
             function_call = getCodeFormat(code)
             print("Function Call")
@@ -147,7 +161,7 @@ if __name__ == '__main__':
 
             responseText_gpt4 = generateCode("gpt-4-1106-preview", userPrompt)
             responseText_gpt3_5 = generateCode("gpt-3.5-turbo-1106", userPrompt)
-
+            print("Generation done")
             # Check the correctness of the code
             success_gpt4 = checkCorrectness(responseText_gpt4, obj)
             success_gpt3_5 = checkCorrectness(responseText_gpt3_5, obj)
@@ -167,6 +181,6 @@ if __name__ == '__main__':
                 break
             
             
-            time.sleep(1)
+            time.sleep(3)
 
 
